f97b97f59169393d5fa34c580e6e329895210678
MUKILTEO\1720941 <1720941@mukilteo.wednet.edu>
2024-10-03 10:23:59 -0700

    making the storie rn

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Jaquavis Studios Presents");
+            Console.WriteLine("Gone Hiking");
+            Console.WriteLine("Type Start To Start");
+
+            string input = Console.ReadLine();
+            if (input == "Start")
+            {
+                Console.WriteLine("(BANG!BANG!BANG!DING!DONG!DING!DONG!BANG!BANG!BANG!)");
+                Console.WriteLine("DUDE GET UP! ITS TIME TO GO! WERE GOING TO BE LATE");
+            }
+
+            Console.ReadKey();
         }
     }
 }
