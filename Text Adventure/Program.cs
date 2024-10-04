da1d196808af8252c17fbdc1000a8cf00ce89268
MUKILTEO\1720941 <1720941@mukilteo.wednet.edu>
2024-10-04 10:21:09 -0700

    add a Darik mode

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Console.Title =("one Hiking");
+
             Console.WriteLine("Jaquavis Studios Presents");
             Console.WriteLine("Gone Hiking");
             Console.WriteLine("Type Start To Start");
@@ -12,10 +14,24 @@
             if (input == "Start")
             {
                 Console.WriteLine("(BANG!BANG!BANG!DING!DONG!DING!DONG!BANG!BANG!BANG!)");
-                Console.WriteLine("DUDE GET UP! ITS TIME TO GO! WERE GOING TO BE LATE");
+                Console.WriteLine("DUDE GET UP! ITS TIME TO GO! WERE GOING TO BE LATE!");
+                Console.WriteLine("Get Up  |  Stay In Bed");
             }
 
+            string input2 = Console.ReadLine();
+            if (input2 == "Get Up")
+            {
+                
+            }
+            else if (input2 == "Stay In Bed")
+            {
+                Console.WriteLine("You got a blood cloat in your brain becuse you were inactive for too long");
+                Console.WriteLine("GAME OVER");
+                Console.WriteLine("Press Any Key To Restart");
+            }
+            
             Console.ReadKey();
+            
         }
     }
 }
