fdb6b668ff1857b6e96d1130c9dfff2d223706a7
MUKILTEO\1720941 <1720941@mukilteo.wednet.edu>
2024-10-08 10:23:08 -0700

    added more to the story+

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -22,13 +22,14 @@
             if (input2 == "get up")
             {
                 Console.WriteLine("Okay okay I'm getting up");
-                Console.WriteLine("just let me get my bag");
-                Console.WriteLine("grab bag  |  go down stairs"); // if the bag is grabed keep going with the story 
+                Console.WriteLine("Just let me get my bag");
+                Console.WriteLine("grab bag  |---------"); // if the bag is grabed keep going with the story 
                 // if the player goes down stairs the player gets to go back up or open the door
             }
             else if (input2 == "stay in bed")
             {
                 Console.WriteLine("You got a blood cloat in your brain becuse you were inactive for too long");
+                Console.WriteLine("----------------------------------");
                 Console.WriteLine("GAME OVER");
                 Console.WriteLine("Press Any Key To Restart");
             }
@@ -40,10 +41,42 @@
                 Console.WriteLine("Okay I should be good to go");
                 Console.WriteLine("go down stairs  |  stay");
             }
-            else if (input3 == "go down stairs")
+            
+            string input4 = Console.ReadLine();
+            if (input4 == "go down stairs")
             {
-                Console.WriteLine("grab gun  |  go to the basement");
+                Console.WriteLine("grab gun  |  go to basement");
             }
+            else if (input4 == "stay")
+            {
+                Console.WriteLine("Hmmmm");
+                Console.WriteLine("HEY GUYS NEVER MIND I'M NOT GOING");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("WHAT THE HECK WE'VE BEEN PLANING THIS FOR AGES");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("You Died of Loneliness Because You No Longer Have Friends");
+            }
+
+            string input5 = Console.ReadLine();
+            if (input5 == "grab gun")
+            {
+                Console.WriteLine("I'll stow this just in case");
+                Console.WriteLine("I guess my only option is to open the door");
+                Console.WriteLine("open door  |--------");
+            }
+            else if (input5 == "go to basement")
+            {
+                Console.WriteLine("You forgot you have a cripling fear of the dark so you quickly run back up");
+                Console.WriteLine("open door  |--------");
+            }
+
+            string input6 = Console.ReadLine();
+            if (input6 == "grab gun")
+            {
+
+            }
+
+
 
             Console.ReadKey();
             
